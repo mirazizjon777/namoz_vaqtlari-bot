c8b5d77051b89164c19e4de378a6e6e48ccca3f9
mirazizjon777 <isroilovmirazizjon@gmail.com>
2023-07-12 16:10:09 +0500

    finished

---
+++ months.js
@@ -0,0 +1,14 @@
+export default [
+  "Yanvar",
+  "Fevral",
+  "Mart",
+  "Aprel",
+  "May",
+  "Iyun",
+  "Iyul",
+  "Avgust",
+  "sentabr",
+  "oktabr",
+  "Noyabr",
+  "Dekabr",
+];
---
+++ regions.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "Toshkent",
+    id: 27,
+  },
+  {
+    name: "Fargona",
+    id: 37,
+  },
+  {
+    name: "Oltiariq",
+    id: 38,
+  },
+  {
+    name: "Rishton",
+    id: 39,
+  },
+  {
+    name: "Quva",
+    id: 40,
+  },
+];
